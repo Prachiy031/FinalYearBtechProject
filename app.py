61078ded644dc6f2d849d62117fa758d86425e6b
prachi2004 <031prachiyadav@gmail.com>
2025-04-13 22:20:42 +0530

    Added req file

--- app.py
+++ app.py
@@ -3,19 +3,18 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 from tensorflow.keras.preprocessing import image 
+import os
 
 app = Flask(__name__)
 
 # Load the trained model
-MODEL_PATH = "C:/Users/Prachi Yadav/Documents/FinalYearProject/FinalYearProject/ImplementationFinalProject/Models/kidney_stone_detection_model_cnn.h5"  
+MODEL_PATH = "Models/kidney_stone_detection_model_cnn.h5"  
 model = tf.keras.models.load_model(MODEL_PATH)
 
-# Route for uploading image
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
     return render_template("index.html")
 
-# Route for prediction
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
@@ -25,25 +24,17 @@ def predict():
     if file.filename == '':
         return "No selected file."
 
-    # Open and convert to RGB just to be safe
     img = Image.open(file.stream).convert("RGB")
-
-    # Resize to match model input size (e.g., 224x224)
     img = img.resize((224, 224))
-
-    # Convert to numpy array and normalize the image (if required)
-    img_array = image.img_to_array(img)  # Convert to array
-    img_array = img_array / 255.0  # Normalize to [0, 1] if the model was trained with normalized data
-
-    # Add batch dimension
+    img_array = image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    # Model handles resizing, scaling, etc.
     prediction = model.predict(img_array)[0]
-    print("Prediction Value: ", prediction)  # Check prediction value here
+    print("Prediction Value: ", prediction)
     
     result = "Kidney Stone Detected" if prediction[0] > 0.5 else "No Kidney Stone"
     return render_template("results.html", prediction=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
